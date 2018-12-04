a1637923093a2940227c984d1bb420fb32cc4145
GDaan2512 <45592397+GDaan2512@users.noreply.github.com>
2018-12-04 11:57:08 +0100

    Update Zone.java

--- Zone.java
+++ Zone.java
@@ -1,20 +1,12 @@
 import java.util.ArrayList;
 
-public class Zone {
+public abstract class Zone {
 	
-	private Parcelle type; 
 	private ArrayList<Carte> cartes;
 	private boolean termine;
 	private int point;
 
 	
-	public Zone(Parcelle type, int point)
-	{
-		this.cartes=new ArrayList<Carte>();
-		this.type=type;
-		this.termine=false;
-		this.point=point;
-	}
 	
 	public void terminer()
 	{
