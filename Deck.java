2d3a70968c1e941da5c08f15e27078301788a776
viviox <45592671+viviox@users.noreply.github.com>
2018-12-14 09:48:18 +0100

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,5 +1,25 @@
-public classe Deck{
-  
+public class Deck {
+    String nbCartes;
+    ArrayList<Carte> carte;
+
+    public Deck() {
+        this.carte = new ArrayList<Carte>();
+        nbCartes = null;
+    }
+
+    public Deck(Carte carte) {
+        this.carte = carte;
+        nbCartes = carte.size();
+    }
+    
+    
+    public void melanger(){
+        
+    }
+    
+    public int getNbCarte(){
+        
+    }
 
 
 }
