0f4ef2053fc6f7185acd111b4c87a3c6a5787eb7
viviox <45592671+viviox@users.noreply.github.com>
2018-12-14 10:43:58 +0100

    Add files via upload

---
+++ Joueur.java
@@ -0,0 +1,26 @@
+public class Joueur {
+    protected String nom;
+    protected int point;
+    protected Couleur couleur;
+    protected Partisan[] partisans;
+
+    public Joueur(String nom, int point, Couleur couleur) {
+        this.nom = nom;
+        this.point = point;
+        this.couleur = couleur;
+        partisans = new Partisan[5];
+    }
+
+
+
+    /*
+     * Les méthodes
+     *
+     *
+     *
+     *
+     *
+     * */
+
+
+}
