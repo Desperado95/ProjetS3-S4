d9e27966ab6476fe3ab0e72b36e0b1bfd955c06d
Desperado95 <43728391+Desperado95@users.noreply.github.com>
2019-01-08 08:36:57 +0100

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,27 +1,44 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Deck {
-    private int nbCartes;
-    ArrayList<Carte> carte;
 
-    public Deck() {
-        this.carte = new ArrayList<Carte>();
-        nbCartes = 0;
+	ArrayList<Carte> lsCarte;
+	private int contient;
+	private int capacity;
+	
+	public Deck() {
+		lsCarte = new ArrayList<Carte>();
+		capacity = 72;
+		contient = 0;
+	}
+	
+	public Boolean AddCarte(Carte c) {
+		if (capacity > contient)
+		{
+			lsCarte.add(c);
+			contient = contient+1;
+			return true;
+		}
+			return false;
+	}
+	
+	public Carte Draw() {
+		contient=contient-1;
+		return  lsCarte.get(1);
+	}
+	
+	public void melanger(){
+        Collections.shuffle(lsCarte);
     }
-
-    public Deck(ArrayList<Carte> carte) {
-        this.carte = carte;
-        nbCartes = carte.size();
-    }
-
-
-    public void melanger() {
-        Collections.shuffle(carte);
-    }
-
-    public int getNbCarte() {
-        return nbCartes;
-    }
-
+	
+	public void Aff() {
+		int i =1;
+		for(Carte c : lsCarte)
+		{
+			System.out.println(i++ +"_____________________");
+			c.Aff();
+		}
+	}
 }
