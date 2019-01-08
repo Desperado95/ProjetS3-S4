9d7ed8f1d9a366722daf1fd01f4f961816ce0bb7
Desperado95 <43728391+Desperado95@users.noreply.github.com>
2019-01-08 08:38:31 +0100

    Update Carte.java

--- Carte.java
+++ Carte.java
@@ -1,56 +1,54 @@
 
 public class Carte {
-    private Parcelle centre;
-    private Parcelle nord;
-    private Parcelle sud;
-    private Parcelle est;
-    private Parcelle ouest;
-    private Etat etat;
+	Parcelle centre;
+	Parcelle nord;
+	Parcelle sud;
+	Parcelle est;
+	Parcelle ouest;
+	Etat etat;
+	
+	public Carte(Parcelle centre, Parcelle nord, Parcelle sud, Parcelle est, Parcelle ouest)
+	{
+		this.centre=centre;
+		this.nord=nord;
+		this.sud=sud;
+		this.est=est;
+		this.ouest=ouest;
+		this.etat=(Etat.non_place);
+	}
+	
+	public Carte(Carte c)
+	{
+		this.centre=c.centre;
+		this.nord=c.nord;
+		this.sud=c.sud;
+		this.est=c.est;
+		this.ouest=c.ouest;
+		this.etat=(Etat.non_place);
+	}
+	
+	public void tournerHoraire()
+	{
+		Parcelle tempo=this.nord;
+		this.nord=this.ouest;
+		this.ouest=this.sud;
+		this.sud=this.est;
+		this.est=tempo;
+	}
+	
+	public void tournerAntiHoraire()
+	{
+		Parcelle tempo=this.nord;
+		this.nord=this.est;
+		this.est=this.sud;
+		this.sud=this.ouest;
+		this.ouest=tempo;
+	}
 
-
-    public Carte() {
-        centre = Parcelle.ville;
-        nord = Parcelle.ville;
-        sud = Parcelle.ville;
-        est = Parcelle.ville;
-        ouest = Parcelle.ville;
-        this.etat = (Etat.non_place);
-    }
-
-
-    public Carte(Parcelle centre, Parcelle nord, Parcelle sud, Parcelle est, Parcelle ouest) {
-        this.centre = centre;
-        this.nord = nord;
-        this.sud = sud;
-        this.est = est;
-        this.ouest = ouest;
-        this.etat = (Etat.non_place);
-    }
-
-    public Carte(Carte carte) {
-        this.centre = carte.centre;
-        this.nord = carte.nord;
-        this.sud = carte.sud;
-        this.est = carte.est;
-        this.ouest = carte.ouest;
-        this.etat = carte.etat;
-    }
-
-
-    public void tournerHoraire() {
-        Parcelle tempo = this.nord;
-        this.nord = this.ouest;
-        this.ouest = this.sud;
-        this.sud = this.est;
-        this.est = tempo;
-    }
-
-    public void tournerAntiHoraire() {
-        Parcelle tempo = this.nord;
-        this.nord = this.est;
-        this.est = this.sud;
-        this.sud = this.ouest;
-        this.ouest = tempo;
-    }
-
+	public void Aff()
+	{
+		System.out.println("	"	+nord +"		");
+		System.out.println(ouest	+"	"	+centre	+"	"	+est);
+		System.out.println("	"+sud+"\n");
+	}
 }
