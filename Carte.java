ca5c9876dcb0c3c5e8a7ce8bcef39a19e7db236b
takamachivivio <specialace11@gmail.com>
2019-01-14 14:51:46 +0100

    update

--- Carte.java
+++ Carte.java
@@ -5,7 +5,20 @@ public class Carte {
     Parcelle sud;
     Parcelle est;
     Parcelle ouest;
+    Parcelle nordest;
+    Parcelle nordouest;
+    Parcelle sudest;
+    Parcelle sudouest;
     Etat etat;
+    Etat eNord;
+    Etat eSud;
+    Etat eOuest;
+    Etat eEst;
+    Etat eNordouest;
+    Etat eNordest;
+    Etat eSudouest;
+    Etat eSudest;
+    Etat eCentre;
 
     public Carte() {
         this.est = null;
@@ -14,6 +27,16 @@ public class Carte {
         ouest = null;
         centre = null;
         this.etat = null;
+
+        eNord = null;
+        eSud = null;
+        eOuest = null;
+        eEst = null;
+        eNordouest = null;
+        eNordest = null;
+        eSudouest = null;
+        eSudest = null;
+        eCentre = null;
     }
 
     public Carte(Parcelle centre, Parcelle nord, Parcelle sud, Parcelle est, Parcelle ouest) {
@@ -23,6 +46,18 @@ public class Carte {
         this.est = est;
         this.ouest = ouest;
         this.etat = (Etat.non_place);
+
+        eNord = Etat.non_place;
+        eSud = Etat.non_place;
+        eOuest = Etat.non_place;
+        eEst = Etat.non_place;
+        eNordouest = Etat.non_place;
+        eNordest = Etat.non_place;
+        eSudouest = Etat.non_place;
+        eSudest = Etat.non_place;
+        eCentre = Etat.non_place;
+        autocomplir();
+
     }
 
     public Carte(Carte c) {
@@ -32,6 +67,17 @@ public class Carte {
         this.est = c.est;
         this.ouest = c.ouest;
         this.etat = (Etat.non_place);
+
+        eNord = c.eNord;
+        eSud = c.eSud;
+        eOuest = c.eOuest;
+        eEst = c.eEst;
+        eNordouest = c.eNordouest;
+        eNordest = c.eNordest;
+        eSudouest = c.eSudouest;
+        eSudest = c.eSudest;
+        eCentre = c.eCentre;
+
     }
 
     public void tournerHoraire() {
@@ -55,4 +101,25 @@ public class Carte {
         System.out.println(ouest + "	" + centre + "	" + est);
         System.out.println("	" + sud + "\n");
     }
+
+    public void autocomplir() {
+        if (centre == Parcelle.chemin) {
+            if (sud == Parcelle.chemin) {
+                if (nord == Parcelle.chemin) {
+                    sudouest = sudest = nordest = nordouest = Parcelle.plaine;
+                } else {
+                    if (ouest == Parcelle.chemin)
+                        sudouest = Parcelle.plaine;
+                    if (est == Parcelle.chemin)
+                        sudest = Parcelle.plaine;
+                }
+            }
+
+
+        }
+
+
+    }
+
+
 }
