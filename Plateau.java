741a8377e5c22886bc6f328b2599d74a970ffaae
Erwann <erwann.forgez@gmail.com>
2019-01-15 11:52:12 +0100

    plateau

---
+++ Plateau.java
@@ -0,0 +1,79 @@
+
+public class Plateau {
+
+	Carte[][] plat;
+	
+	Plateau(Carte c){
+		plat = new Carte[72][72];
+		plat[35][35]=c;
+	}
+	
+	Boolean ajouter(Carte c,int x, int y) {
+		if(plat[x][y]!=null)
+		{
+			System.out.println("1");
+			return false;
+			
+		}
+		else
+		{	if(plat[x+1][y]!=null)
+			{
+				if(plat[x+1][y].ouest==c.est)
+				{
+					System.out.println("2 valentin was here");
+					return false;
+				}
+			}
+			 if(plat[x-1][y]!=null) 
+			{
+				if(plat[x-1][y].est==c.ouest)
+				{
+					System.out.println("3");
+					return false;
+				}
+			}
+			 if(plat[x][y+1]!=null) 
+			{
+				if(plat[x][y+1].nord==c.sud)
+				{
+					System.out.println("4");
+					return false;
+				}
+			}
+			 if(plat[x][y-1]!=null) 
+			{
+				if(plat[x][y-1].sud==c.nord)
+				{
+					System.out.println("5");
+					return false;
+				}
+			}
+				System.out.println("6");
+				plat[x][y]=c;
+				return true;
+			
+		}
+		
+	}
+	
+	void afficher(){
+		Carte c;
+		for(int i=0;i<72;i++) 
+		{
+			System.out.print(i);
+			for(int y=0;y<72;y++) 
+			{
+				
+				if(plat[i][y]==null) {
+				System.out.print("|		vide		|");
+				}
+				else
+				{
+					c =plat[i][y];
+					System.out.print(c.nord +"|"+plat[i][y].ouest+"|"+plat[i][y].est+"|"+plat[i][y].sud);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
