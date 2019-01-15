963567720477ae04e7a0a464ebd4c2f48cf55bcb
takamachivivio <specialace11@gmail.com>
2019-01-15 12:03:17 +0100

    updatejoueur

--- Joueur.java
+++ Joueur.java
@@ -25,10 +25,85 @@ public class Joueur {
     }
 
     /*Le methode de mettre un partisant sur la carte ce qui est dans le main*/
-    public void mettreLesPions() {
-        if (!partisans.isEmpty()) {
-        	
+    public void mettreLesPions(int i) {
+        public void mettreLesPions () {
+            if (!partisans.isEmpty()) {
+                switch (i) {
+                    case 1:
+                        if (carte.eNordouest != Etat.disable) {
+                            carte.eNordouest = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 2:
+                        if (carte.eNord != Etat.disable) {
+                            carte.eNord = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 3:
+                        if (carte.eNordest != Etat.disable) {
+                            carte.eNordest = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 4:
+                        if (carte.eEst != Etat.disable) {
+                            carte.eEst = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 5:
+                        if (carte.eSudest != Etat.disable) {
+                            carte.eSudest = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 6:
+                        if (carte.eSud != Etat.disable) {
+                            carte.eSud = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 7:
+                        if (carte.eSudouest != Etat.disable) {
+                            carte.eSudouest = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 8:
+                        if (carte.eOuest != Etat.disable) {
+                            carte.eOuest = Etat.place;
+                            parcourirPlace();
+                        }
+                    case 9:
+                        if (carte.eCentre != Etat.disable) {
+                            carte.eCentre = Etat.place;
+                            parcourirPlace();
+                        }
+
+
+                }
+            }
         }
+
+        /* Parcourir tousles états,est ce qu'il exist un état == placé*/
+
+        public void parcourirPlace () {
+            if (carte.eNord == Etat.place)
+                carte.eNord = Etat.non_place;
+            if (carte.eSud == Etat.place)
+                carte.eSud = Etat.non_place;
+            if (carte.eOuest == Etat.place)
+                carte.eOuest = Etat.non_place;
+            if (carte.eEst == Etat.place)
+                carte.eEst = Etat.non_place;
+            if (carte.eNordouest == Etat.place)
+                carte.eNordouest = Etat.non_place;
+            if (carte.eNordest == Etat.place)
+                carte.eNordest = Etat.non_place;
+            if (carte.eSudouest == Etat.place)
+                carte.eSudouest = Etat.non_place;
+            if (carte.eSudest == Etat.place)
+                carte.eSudest = Etat.non_place;
+            if (carte.eCentre == Etat.place)
+                carte.eCentre = Etat.non_place;
+        }
+
+
     }
 
 
