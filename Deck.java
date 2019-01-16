3dc5b32e320f5214cd913a73a82a678e51841536
takamachivivio <specialace11@gmail.com>
2019-01-16 17:18:37 +0100

    update deck

--- Deck.java
+++ Deck.java
@@ -4,12 +4,11 @@ import java.util.Random;
 
 public class Deck {
 
-	ArrayList<Carte> lsCarte;
-	private int contient;
-	private int capacity;
-	
-	
-		
+    ArrayList<Carte> lsCarte;
+    private int contient;
+    private int capacity;
+
+
     public Deck() {
         lsCarte = new ArrayList<Carte>();
         capacity = 72;
@@ -18,151 +17,155 @@ public class Deck {
     }
 
     private void RemplirDeck() {
-    
+        Carte c1 = new Carte(Parcelle.chemin, Parcelle.plaine, Parcelle.chemin, Parcelle.plaine, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         int i = 9;
         for (int y = 0; y < i; y++) {
-
-            AddCarte(new Carte(Parcelle.chemin, Parcelle.plaine, Parcelle.chemin, Parcelle.plaine, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine););
-
+            AddCarte(c1);
         }
-
+        Carte c2 = new Carte(Parcelle.chemin, Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine);
         i = 3;
         for (int y = 0; y < i; y++) {
-
-            AddCarte(new Carte(Parcelle.chemin, Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine););
+            AddCarte(c2);
         }
-    
+        Carte c3 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.chemin);
         i = 2;
         for (int y = 0; y < i; y++) {
-            AddCarte( new Carte(ville, ville, chemin, chemin, ville, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine););
+            AddCarte(c3);
         }
-        
+        Carte c4 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.chemin, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 1;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, chemin, ville, ville););
+            AddCarte(c4);
         }
-        
+        Carte c5 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 1;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, plaine, ville, ville););
+            AddCarte(c5);
         }
+        Carte c6 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.plaine);
         i = 3;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, chemin, chemin, ville););
+            AddCarte(c6);
         }
+        Carte c7 = new Carte(Parcelle.chemin, Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.chemin, Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine);
         i = 3;
         for (int y = 0; y < i; y++) {
-            AddCarte( new Carte(chemin, ville, chemin, chemin, chemin););
+            AddCarte(c7);
         }
+        Carte c8 = new Carte(Parcelle.chemin, Parcelle.chemin, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 8;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(chemin, chemin, chemin, plaine, plaine););
+            AddCarte(c8);
         }
+        Carte c9 = new Carte(Parcelle.ville, Parcelle.plaine, Parcelle.chemin, Parcelle.chemin, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 4;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, plaine, chemin, chemin, chemin););
+            AddCarte(c9);
         }
-         
+        Carte c10 = new Carte(Parcelle.plaine, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 5;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(plaine, ville, plaine, plaine, plaine););
+            AddCarte(c10);
         }
-        
+        Carte c11 = new Carte(Parcelle.plaine, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.ville, Parcelle.plaine, Parcelle.ville, Parcelle.plaine, Parcelle.plaine);
         i = 2;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(plaine, ville, plaine, plaine, ville););
+            AddCarte(c11);
         }
+        Carte c12 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 3;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, plaine, ville, ville););
+            AddCarte(c12);
         }
+        Carte c13 = new Carte(Parcelle.abbaye, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 4;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(abbaye, plaine, plaine, plaine, plaine););
+            AddCarte(c13);
         }
-       
+        Carte c14 = new Carte(Parcelle.ville, Parcelle.plaine, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 2;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, plaine, chemin, plaine, plaine););
+            AddCarte(c14);
         }
-        
+        Carte c15 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 3;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, plaine, plaine, ville););
+            AddCarte(c15);
         }
-        
+        Carte c16 = new Carte(Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 2;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, plaine, plaine, ville, ville););
+            AddCarte(c16);
         }
-        
+        Carte c17 = new Carte(Parcelle.chemin, Parcelle.ville, Parcelle.plaine, Parcelle.chemin, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 4;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(chemin, ville, plaine, chemin, chemin););
+            AddCarte(c17);
         }
-       
+        Carte c18 = new Carte(Parcelle.chemin, Parcelle.ville, Parcelle.chemin, Parcelle.plaine, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 3;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(chemin, ville, chemin, plaine, chemin););
+            AddCarte(c18);
         }
-        
+        Carte c19 = new Carte(Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 1;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, plaine, plaine, ville, ville););
+            AddCarte(c19);
         }
-      
+        Carte c20 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.chemin, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 2;
         for (int y = 0; y < i; y++) {
-            AddCarte(  new Carte(ville, ville, chemin, chemin, chemin););
+            AddCarte(c20);
         }
-        
+        Carte c21 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville, Parcelle.ville);
         i = 1;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, ville, ville, ville););
+            AddCarte(c21);
         }
-        
+        Carte c22 = new Carte(Parcelle.ville, Parcelle.chemin, Parcelle.chemin, Parcelle.chemin, Parcelle.chemin, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 1;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, chemin, chemin, chemin, chemin););
+            AddCarte(c22);
         }
+        Carte c23 = new Carte(Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 2;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(ville, ville, plaine, plaine, ville););
+            AddCarte(c23);
         }
+        Carte c24 = new Carte(Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.ville, Parcelle.ville, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine, Parcelle.plaine);
         i = 3;
         for (int y = 0; y < i; y++) {
-            AddCarte(new Carte(plaine, plaine, plaine, ville, ville););
+            AddCarte(c24);
         }
-	}
-	
-	public Boolean AddCarte(Carte c) {
-		if (capacity > contient)
-		{
-			lsCarte.add(c);
-			contient = contient+1;
-			return true;
-		}
-			return false;
-	}
-	
-	public Carte Draw() {
-		contient=contient-1;
-		return  lsCarte.get(1);
-	}
-	
-	public void melanger(){
+    }
+
+    public Boolean AddCarte(Carte c) {
+        if (capacity > contient) {
+            lsCarte.add(c);
+            contient = contient + 1;
+            return true;
+        }
+        return false;
+    }
+
+    public Carte Draw() {
+        contient = contient - 1;
+        return lsCarte.get(1);
+    }
+
+    public void melanger() {
         Collections.shuffle(lsCarte);
     }
-	
-	public void Aff() {
-		int i =1;
-		for(Carte c : lsCarte)
-		{
-			System.out.println(i++ +"_____________________");
-			c.Aff();
-		}
-	}
-	public boolean isEmpty() {
-		return lsCarte.isEmpty();
-		}
+
+    public void Aff() {
+        int i = 1;
+        for (Carte c : lsCarte) {
+            System.out.println(i++ + "_____________________");
+            c.Aff();
+        }
+    }
+
+    public boolean isEmpty() {
+        return lsCarte.isEmpty();
+    }
 }
