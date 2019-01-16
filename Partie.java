4f8fc32835dc088045510997160eb4c3f8d8c893
Desperado95 <43728391+Desperado95@users.noreply.github.com>
2019-01-16 16:03:23 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -1,16 +1,39 @@
-import java.util.ArrayList;
 
 public class Partie {
 
 	public ArrayList<Joueur> joueurs;
 	public Plateau plat;
 	public Deck d;
-
+	
 	public Partie() {
 		joueurs = new ArrayList<Joueur>();
 		d = new Deck();
 		plat = new Plateau(d.Draw());
 	}
 
-
+	
+	public void jouer() {
+		while(!d.isEmpty)
+		{
+			tour();
+		}
+	}
+	
+	public void tour() {
+		for(Joueur j : joueurs)
+		{
+			if(!d.isEmpty) {
+				tourJoueur(j);
+			}
+			else {
+				break;
+			}
+		}
+	}
+	
+	public void tourJoueur(Joueur j) {
+		Carte c= d.Draw();
+		j.c = c;
+		
+	}
 }
